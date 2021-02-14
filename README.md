7c54e4a213492745485f05d25dfff8bef4e71ed0
kauffmza <kauffmza@oregonstate.edu>
2021-02-13 23:12:10 -0800

    pog

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ To compile:
 
 `g++ -std=c++11 main.cpp -o main`
 
-To run:
+NOTE: Program expects the name of the employee CSV to be "Employees.csv"
+If your file name is "Employee.csv" please go to line 208 and change the getEmployeesFromFile() parameter appropriately
 
-`./main`
+To create the employee index:
+
+`./main C`
+
+To locate an employee by ID:
+
+`./main L [ID]`
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+//Zach Kauffman and David Kalcic
+//For OSU CS-440 Winter Term 2021
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,6 +15,7 @@
 
 using namespace std;
 
+//employee class contains info for the employee
 class Employee{
 public:
     string id, name, bio, managerId;
@@ -22,34 +26,37 @@ public:
 };
 
 Employee::Employee(string id, string name, string bio, string managerId){
-    // copy values into bucket up to the maximum size
+    //store values only up to maximum size
     this->id = id.substr(0, ID_SIZE);
     this->managerId = managerId.substr(0, ID_SIZE);
     this->bio = bio.substr(0, BIO_SIZE);
     this->name = name.substr(0, NAME_SIZE);
 }
 
+//returns the size of the employee in bytes
 int Employee::getSize(){
     return id.length() + name.length() + bio.length() + managerId.length();
 }
 
+//a block holds employees up to 4096 bytes
 class Block{
 public:
 
+    //contains a vector for employees up to 4096 bytes
+    //and an overflow vector for employees past that size
     vector<Employee> employees, overflow;
     int modFactor;
 
     void insert(Employee);
 
     int getTotalSize();
-
     int getSizeNoOverflow();
 
     Block();
-
     Block(int);
 };
 
+//default constructor
 Block::Block(){
     modFactor = 2;
 }
@@ -58,19 +65,17 @@ Block::Block(int modFactor){
     this->modFactor = modFactor;
 }
 
+//inserts an employee into the block or overflow depending on remaining space
 void Block::insert(Employee emp){
-    cout << "---block size: " << getSizeNoOverflow() << endl;
-    cout << "---emp size: " << emp.getSize() << endl;
-    // make sure to enforce block size (create overflow if already full)
+    //checks if current size + employee size will go past 4096 bytes
     if(getSizeNoOverflow() + emp.getSize() > BLOCK_SIZE){
-        cout << "---Inserting " << emp.id << " into overflow" << endl;
         overflow.push_back(emp);
     } else{
-        cout << "---Inserting " << emp.id << " into employees" << endl;
         employees.push_back(emp);
     }
 }
 
+//get the total size of just non-overflow employees in the block
 int Block::getSizeNoOverflow(){
     int sum = 0;
     for(int ii = 0; ii < employees.size(); ii++){
@@ -79,6 +84,7 @@ int Block::getSizeNoOverflow(){
     return sum;
 }
 
+//get the total size of all employees in block and overflow
 int Block::getTotalSize(){
     int sum = getSizeNoOverflow();
     if(!overflow.empty()){
@@ -89,6 +95,7 @@ int Block::getTotalSize(){
     return sum;
 }
 
+//hashtable holds blocks
 class HashTable{
 public:
     int n; //n = current block
@@ -98,16 +105,19 @@ public:
     HashTable();
 
     void insert(Employee); //checks if block has space, if not: splits first
-    void split(int); //creates a new block and distributes data
+    void split(); //creates a new block and distributes data
     int getTableSize();
 };
 
+//default constructor
+//contains two empty blocks by default
 HashTable::HashTable(){
     n = 0;
     blocks = {{},
               {}};
 }
 
+//returns total size of contents of all blocks, including overflow
 int HashTable::getTableSize(){
     int sum = 0;
     for(int ii = 0; ii < blocks.size(); ii++){
@@ -116,36 +126,41 @@ int HashTable::getTableSize(){
     return sum;
 }
 
+//finds which block to insert an employee into and then inserts it
+//if block is full, inserts employee into overload
+//if split parameter is hit (80%), split at index n
+//if split is performed, increment n
 void HashTable::insert(Employee emp){
+    //calculates current mod factor
     int modFactor = pow(2, ceil(log2(blocks.size())));
-    cout << "Mod factor " << modFactor << endl;
     int id = stoi(emp.id);
-    int index = id % modFactor;
+    int index = id % modFactor; //calculates proper index for the employee by id mod modfactor
 
+    //ensures modfactor doesn't get too big 
+    //i don't know why i had to do this
     if(index >= blocks.size()){
-        cout << "Just kidding... Mod factor " << modFactor / 2 << endl;
         index = id % (modFactor / 2);
     }
 
-    cout << "Inserting in block #" << index << " ...\n";
+    //inserts employee at proper index (or overflow)
     blocks[index].insert(emp);
 
+    //checks if split parameter has been hit
     if(((float) getTableSize() / (float) (blocks.size() * BLOCK_SIZE)) > SPLIT_PARAMETER){
-        split(index);
+        split();
     }
 }
 
-void HashTable::split(int index){
-    // This calculates the modFactor
+void HashTable::split(){
+    //modfactor for the split needs to be calculated at current size + 1, since a new block will be added
     int modFactor = pow(2, ceil(log2(blocks.size() + 1)));
     int newIndex = 0, overflowSize = 0;
-    int blockSize = blocks[n].employees.size();
+    int blockSize = blocks[n].employees.size(); //number of employees
 
-    cout << "|||Splitting on block " << n << endl;
+    blocks.push_back(Block(modFactor));//insert a new block
+    blocks[n].modFactor = modFactor;//increment modfactor at block n
 
-    blocks.push_back(Block(modFactor));
-    blocks[n].modFactor = modFactor;
-
+    //redistributes employees from block n as necessary
     for(int ii = 0; ii < blockSize; ii++){
         newIndex = stoi(blocks[n].employees.back().id) % blocks[n].modFactor;
         if(newIndex != n){
@@ -155,9 +170,9 @@ void HashTable::split(int index){
             blocks[n].employees.insert(blocks[n].employees.begin(), blocks[n].employees.back());
         }
         blocks[n].employees.pop_back();
-
     }
 
+    //if there are employees in overflow, redistribute them
     if(!blocks[n].overflow.empty()){
         overflowSize = blocks[n].overflow.size();
         for(int ii = 0; ii < overflowSize; ii++){
@@ -173,20 +188,20 @@ void HashTable::split(int index){
         }
     }
 
+    //more modfactor jank
     blocks[n].modFactor /= 2;
 
-    cout << "Your new n is ... ";
+    //increments modfactor to either n+1 or 0
     if(blocks[n].modFactor == blocks[n + 1].modFactor){
         n++;
     } else{
         n = 0;
     }
-    cout << n << "!!!\n";
-
-    blocks[n].modFactor = modFactor;
+    blocks[n].modFactor = modFactor; //the last jank i promise
 
 }
 
+//reads employee data from a csv and returns a vector of the Employee class
 vector<Employee> getEmployeesFromFile(string filename){
     vector<Employee> employees;
     string id, name, bio, managerId;
@@ -207,7 +222,7 @@ int main(int argc, char* argv[]){
     HashTable h;
     vector<Employee> employees = getEmployeesFromFile("Employees.csv");
     if(string(argv[1]) == "C"){
-        ofstream myfile("EmployeeIndex");
+        ofstream myfile("EmployeeIndex.txt");
         if(myfile.is_open()){
             for(int i = 0; i < employees.size(); i++){
                 h.insert(employees[i]);
@@ -223,23 +238,29 @@ int main(int argc, char* argv[]){
                 myfile << endl;
             }
             myfile.close();
+            cout << "Wrote to EmployeeIndex.txt\n";
+            return 0;
+        } else {
+            cout << "Error opening file\n";
+            return 1;
         }
-
-
     }
-    if(argv[1] == "L"){
+    if(string(argv[1]) == "L"){
         if(argc != 3){
             cout << "MISSING ID ARG" << endl;
         } else{
+            cout << "Locating employee...\n";
             for(int ii = 0; ii < employees.size(); ii++){
                 if(employees[ii].id == string(argv[2])){
                     cout << "Employee id: " << employees[ii].id << endl;
                     cout << "Name: " << employees[ii].name << endl;
                     cout << "Bio: " << employees[ii].bio << endl;
                     cout << "Manager id: " << employees[ii].managerId << endl;
+                    return 0;
                 }
             }
+            cout << "Couldn't find an employee with that ID\n";
+            return 1;
         }
     }
 }
-
